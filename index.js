d4c1aefe2818ae54f9efe71693be6d34978976cf
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-07-06 11:40:37 +0100

    Add script

---
+++ index.js
@@ -0,0 +1,10 @@
+const adviceId = document.getElementById('adviceId');
+const adviceText = document.getElementById('adviceText');
+
+const handleClick = async()=> {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    console.log(data);
+    adviceId.innerHTML = data.slip.id;
+    adviceText.innerHTML = data.slip.advice;
+}
