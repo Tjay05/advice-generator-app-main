2c9d4a496428162875a1a43c0bf87bb8205066a0
Tjay05 <tjayolatayo@gmail.com>
2024-10-31 13:27:42 +0100

    Edit res

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const adviceId = document.getElementById('adviceId');
 const adviceText = document.getElementById('adviceText');
 
 const handleClick = async()=> {
-    const res = await fetch('https://api.adviceslip.com/advice');
+    const response = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
     
     adviceId.innerHTML = data.slip.id;
